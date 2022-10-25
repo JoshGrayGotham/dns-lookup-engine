c7376d630fa8346c188c91de04722dee08390129
JoshGrayGotham <116634579+JoshGrayGotham@users.noreply.github.com>
2022-10-25 12:56:00 +0100

    Add files via upload

---
+++ multi-lookup.c
@@ -0,0 +1,277 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include "util.h"
+#include "queue.h"
+
+#define MINARGS 3
+#define USAGE "<inputFilePath> <outputFilePath>"
+#define SBUFSIZE 1025
+#define INPUTFS "%1024s"
+#define TEST_SIZE 10
+#define NUM_THREADS 10
+
+// Thread Data Structures
+struct RequestData{
+    long threadNumber;
+    FILE* inputFile;
+    queue* q;
+};
+struct ResolveData{
+    FILE* outputFile;
+    queue* q;
+};
+
+// Thread Mutexes
+pthread_mutex_t mutex;
+pthread_mutex_t writeBlock;
+pthread_mutex_t readBlock;
+pthread_mutex_t queueBlock;
+pthread_mutex_t reportBlock;
+
+int requestThreadsComplete = 0;
+int readCount = 0;
+int debug = 1;
+
+
+void* RequestThread(void* threadarg){
+    struct RequestData * requestData;
+    char hostname[SBUFSIZE];
+    FILE* inputfp;
+    queue* q;
+    char* payload;
+    long threadNumber;
+
+    requestData = (struct RequestData *) threadarg;
+    inputfp = requestData->inputFile;
+    q = requestData->q;
+    threadNumber = requestData->threadNumber;
+
+    // Open Input File
+    if(!inputfp){
+        printf("Error Opening Input File");
+        pthread_exit(NULL);
+    }
+
+    // Add to queue
+    if (debug){
+    	printf("File being read\n");
+    }
+    while(fscanf(inputfp, INPUTFS, hostname) > 0){
+        int completed = 0;
+        while(!completed){
+            // Check if queue is full
+            pthread_mutex_lock(&readBlock);
+            pthread_mutex_lock(&writeBlock);
+            if (!queue_is_full(q)){
+                payload = malloc(SBUFSIZE);
+                strncpy(payload, hostname, SBUFSIZE);
+
+                if(debug){
+                	printf("Adding Payload to Queue:%s\n", payload);
+                }
+                queue_push(q, payload);
+                completed = 1;
+            }
+            // release queue
+            pthread_mutex_unlock(&writeBlock);
+            pthread_mutex_unlock(&readBlock);
+
+            // Wait if not completed
+            if (!completed){
+                usleep((rand()%100)*100000);
+            }
+        }
+        //release queue
+    }
+    fclose(inputfp);
+
+    if(debug){
+    	printf("Completing Request thread %ld\n", threadNumber);
+	}
+    return NULL;
+}
+
+void* ResolveThread(void* threadarg){
+    struct ResolveData * resolveData;
+    FILE* outputfp;
+    char firstipstr[INET6_ADDRSTRLEN];
+    char * hostname;
+    queue* q;
+
+
+    resolveData = (struct ResolveData *) threadarg;
+    outputfp = resolveData->outputFile;
+    q = resolveData->q;
+
+    int emptyQueue = 0;
+    while (!emptyQueue || !requestThreadsComplete){
+    	int resolved = 0;
+        pthread_mutex_lock(&readBlock);
+        pthread_mutex_lock(&mutex);
+        readCount++;
+        if (readCount==1){
+            pthread_mutex_lock(&writeBlock);
+        }
+        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&readBlock);
+
+        // Pop off the queue one at a time
+        pthread_mutex_lock(&queueBlock);
+        emptyQueue = queue_is_empty(q);
+        if(!emptyQueue){
+            hostname = queue_pop(q);
+            if (hostname != NULL){
+            	if(debug){
+            		printf("Reading From Queue: %s\n", hostname);
+            	}
+	            resolved = 1;
+        	}
+        }
+        pthread_mutex_unlock(&queueBlock);
+
+        pthread_mutex_lock(&mutex);
+        readCount--;
+        if (readCount == 0){
+            pthread_mutex_unlock(&writeBlock);
+        }
+        pthread_mutex_unlock(&mutex);
+
+        if(resolved){
+        	queue dnsList;
+        	queue_init(&dnsList, 50);
+            if(dnslookupall(hostname, firstipstr, sizeof(firstipstr), &dnsList) == UTIL_FAILURE){
+                fprintf(stderr, "dnslookup error: %s\n", hostname);
+                strncpy(firstipstr, "", sizeof(firstipstr));
+            }
+            fprintf(outputfp, "%s", hostname);
+    		char * element;
+    		pthread_mutex_lock(&reportBlock);
+            while( (element = (char *) queue_pop(&dnsList)) != NULL){
+		    	fprintf(outputfp, ",%s", element);
+		    	free(element);
+		    }
+		    fprintf(outputfp, "\n");
+            pthread_mutex_unlock(&reportBlock);
+		    free(hostname);
+		    queue_cleanup(&dnsList);
+        }
+
+    }
+
+    if(debug){
+    	printf("Complete Resolve thread\n");
+    }
+
+    return NULL;
+}
+
+
+int main(int argc, char* argv[]){
+	/* Local Vars */
+    FILE* outputfp = NULL;
+
+    /* Check Arguments */
+    if(argc < MINARGS){
+		fprintf(stderr, "Not enough arguments: %d\n", (argc - 1));
+		fprintf(stderr, "Usage:\n %s %s\n", argv[0], USAGE);
+		return EXIT_FAILURE;
+    }
+    if (argc > NUM_THREADS + 1){
+    	fprintf(stderr, "Too many files: %d\n", (argc - 2));
+    	fprintf(stderr, "Usage:\n %s %s\n", argv[0], USAGE);
+		return EXIT_FAILURE;
+    }
+
+    /* Open Output File */
+    outputfp = fopen(argv[(argc-1)], "w");
+    if(!outputfp){
+		perror("Error Opening Output File");
+		return EXIT_FAILURE;
+    }
+
+    // Build Queue
+    queue q;
+    const int qSize = TEST_SIZE;
+    if(queue_init(&q, qSize) == QUEUE_FAILURE){
+		fprintf(stderr, "error: queue_init failed!\n");
+    }
+
+
+    /////////////////////////////////////////////////
+    // Create REQUEST Thread Pool
+    /////////////////////////////////////////////////
+    struct RequestData requestData[NUM_THREADS];
+    pthread_t requestThreads[NUM_THREADS];
+    int rc;
+    long t;
+
+    /* Spawn REQUEST threads */
+    for(t=0; t<(argc-2) && t<NUM_THREADS; t++){
+        requestData[t].q = &q;
+        requestData[t].inputFile = fopen(argv[t+1], "r");
+        requestData[t].threadNumber = t;
+        if (debug){
+        	printf("Creating REQUEST Thread %ld\n", t);
+        }
+		rc = pthread_create(&(requestThreads[t]), NULL, RequestThread, &(requestData[t]));
+		if (rc){
+		    printf("ERROR; return code from pthread_create() is %d\n", rc);
+		    exit(EXIT_FAILURE);
+		}
+    }
+
+
+    /////////////////////////////////////////////////
+    // Create RESOLVE Thread Pool
+    /////////////////////////////////////////////////
+    struct ResolveData resolveData;
+    int cpuCoreCount = sysconf(_SC_NPROCESSORS_ONLN);
+    pthread_t resolveThreads[cpuCoreCount];
+    int rc2;
+    long t2;
+
+    resolveData.q = &q;
+    resolveData.outputFile = outputfp;
+
+    /* Spawn RESOLVE threads */
+
+    for(t2=0; t2<cpuCoreCount; t2++){
+    	if (debug){
+    		printf("Creating RESOLVER Thread %ld\n", t2);
+    	}
+        rc2 = pthread_create(&(resolveThreads[t2]), NULL, ResolveThread, &resolveData);
+        if (rc2){
+            printf("ERROR; return code from pthread_create() is %d\n", rc2);
+            exit(EXIT_FAILURE);
+        }
+    }
+    //Join Threads to detect completion
+    for(t=0; t<(argc-2) && t<NUM_THREADS; t++){
+        pthread_join(requestThreads[t],NULL);
+    }
+    if (debug){
+    	printf("All Request threads have FINISHED!\n");
+    }
+    requestThreadsComplete = 1;
+
+    //Join Threads to detect completion
+    for(t=0; t<cpuCoreCount; t++){
+        pthread_join(resolveThreads[t],NULL);
+    }
+    if (debug){
+    	printf("All Resolver threads have FINISHED!\n");
+    }
+
+    /* Close Output File */
+    fclose(outputfp);
+
+    /* Cleanup Queue */
+    queue_cleanup(&q);
+
+    return EXIT_SUCCESS;
+}
---
+++ util.c
@@ -0,0 +1,118 @@
+#include "util.h"
+
+#define MAX_DNS_RESULTS 50
+
+void addToDnsList(queue* q, char * ip){
+	char * ipAddress;
+
+	int i;
+    for(i = 0; i < q->rear; i++){
+    	if (!strcmp(ip,(char *) q->array[q->front + i].payload)){
+    		return;
+    	}
+    }
+
+	ipAddress = malloc(INET6_ADDRSTRLEN);
+	strncpy(ipAddress, ip, INET6_ADDRSTRLEN);
+	queue_push(q, ipAddress);
+}
+
+int dnslookup(const char* hostname, char* firstIPstr, int maxSize){
+	return dnslookupall(hostname, firstIPstr, maxSize, NULL);
+}
+
+int dnslookupall(const char* hostname, char* firstIPstr, int maxSize, queue* q){
+
+    /* Local vars */
+    struct addrinfo* headresult = NULL;
+    struct addrinfo* result = NULL;
+    struct sockaddr_in* ipv4sock = NULL;
+    struct sockaddr_in6* ipv6sock = NULL;
+    struct in_addr* ipv4addr = NULL;
+    struct in6_addr* ipv6addr = NULL;
+    char ipv4str[INET_ADDRSTRLEN];
+    char ipv6str[INET6_ADDRSTRLEN];
+    char ipstr[INET6_ADDRSTRLEN];
+    int addrError = 0;
+
+    /* DEBUG: Print Hostname*/
+	#ifdef UTIL_DEBUG
+    	fprintf(stderr, "%s\n", hostname);
+	#endif
+
+   	// queue_init(&q, MAX_DNS_RESULTS);
+   
+    /* Lookup Hostname */
+    addrError = getaddrinfo(hostname, NULL, NULL, &headresult);
+    if(addrError){
+		fprintf(stderr, "Error looking up Address: %s\n",
+			gai_strerror(addrError));
+		return UTIL_FAILURE;
+    }
+    /* Loop Through result Linked List */
+    for(result=headresult; result != NULL; result = result->ai_next){
+		/* Extract IP Address and Convert to String */
+		if(result->ai_addr->sa_family == AF_INET){
+		    /* IPv4 Address Handling */
+		    ipv4sock = (struct sockaddr_in*)(result->ai_addr);
+		    ipv4addr = &(ipv4sock->sin_addr);
+		    if(!inet_ntop(result->ai_family, ipv4addr,
+				  ipv4str, sizeof(ipv4str))){
+				perror("Error Converting IP to String");
+				return UTIL_FAILURE;
+			}
+
+			#ifdef UTIL_DEBUG
+			    fprintf(stdout, "%s\n", ipv4str);
+			#endif
+		    strncpy(ipstr, ipv4str, sizeof(ipstr));
+		    ipstr[sizeof(ipstr)-1] = '\0';
+
+		    if(q != NULL){
+		    	addToDnsList(q, ipv4str);
+		    }
+		    
+		}
+		else if(result->ai_addr->sa_family == AF_INET6){
+	    	/* IPv6 Handling */
+
+			ipv6sock = (struct sockaddr_in6*)(result->ai_addr);
+			ipv6addr = &(ipv6sock->sin6_addr);
+
+			if(!inet_ntop(result->ai_family, ipv6addr,
+				  ipv6str, sizeof(ipv6str))){
+				perror("Error Converting IP to String");
+				return UTIL_FAILURE;
+			}
+
+			#ifdef UTIL_DEBUG
+			    fprintf(stdout, "IPv6 Address: %s\n", ipv6str);
+			#endif
+
+		    // strncpy(ipstr, "UNHANDELED", sizeof(ipstr));
+		    // ipstr[sizeof(ipstr)-1] = '\0';
+		    if(q != NULL){
+		    	addToDnsList(q, ipv6str);
+		    }
+		}
+		else{
+	    	/* Unhandlded Protocol Handling */
+			#ifdef UTIL_DEBUG
+			    fprintf(stdout, "Unknown Protocol: Not Handled\n");
+			#endif
+		    strncpy(ipstr, "UNHANDELED", sizeof(ipstr));
+		    ipstr[sizeof(ipstr)-1] = '\0';
+		}
+		/* Save First IP Address */
+		if(result==headresult){
+		    strncpy(firstIPstr, ipstr, maxSize);
+		    firstIPstr[maxSize-1] = '\0';
+		}
+		
+    }
+    
+    /* Cleanup */
+    freeaddrinfo(headresult);
+
+    return UTIL_SUCCESS;
+}
